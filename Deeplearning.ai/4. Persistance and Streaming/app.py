5130069a7a1a80ca592a797e3122dfc850e34d5e
FabioDiCeglie <dcfabio96@gmail.com>
2025-05-22 22:31:45 +0200

    refactor: clean up imports in app.py by removing unused TypedDict and operator

--- Deeplearning.ai/4. Persistance and Streaming/app.py
+++ Deeplearning.ai/4. Persistance and Streaming/app.py
@@ -3,9 +3,7 @@ from dotenv import load_dotenv
 _ = load_dotenv()
 
 from langgraph.graph import StateGraph, END
-from typing import TypedDict, Annotated
-import operator
-from langchain_core.messages import AnyMessage, SystemMessage, HumanMessage, ToolMessage
+from langchain_core.messages import SystemMessage, HumanMessage, ToolMessage
 from langchain_openai import ChatOpenAI
 from langchain_community.tools.tavily_search import TavilySearchResults
 
